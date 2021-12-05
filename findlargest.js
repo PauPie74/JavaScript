aadbcd442c9b9ea63415c0161fee588310df1ad9
Paulina <79440830+PauPie74@users.noreply.github.com>
2021-12-05 14:26:43 +0100

    Add files via upload

---
+++ findlargest.js
@@ -0,0 +1,17 @@
+function finlargest(n1,n2,n3,n4,n5) {
+  let largest = n1
+  if (n2 > largest) {
+  largest = n2
+  }
+  if (n3 > largest) {
+  largest = n3
+  }
+  if (n4 > largest) {
+  largest = n4
+  }
+  if (n5 > largest) {
+  largest = n5
+  }
+
+  console.log(largest)
+}
