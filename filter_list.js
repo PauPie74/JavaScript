ebfa767b300696f464ce2274d2be06c485a85eb4
Paulina <79440830+PauPie74@users.noreply.github.com>
2021-12-17 21:03:00 +0100

    Add files via upload

---
+++ filter_list.js
@@ -0,0 +1,13 @@
+function filter_list(l) {
+  let filtered = []
+  for (let i = 0; i < l.length; i++) {
+    if (typeof l[i] != 'string') {
+      filtered.push(l[i]);
+    }
+  }
+  return filtered
+}
+
+
+
+filter_list([1,2,'a','b'])
